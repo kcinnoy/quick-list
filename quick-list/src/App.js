a8168137ac2ba6b5921936fe65a3d892a195642f
kcinnoy <yonnick00@gmail.com>
2019-11-22 19:12:48 +0000

    test reducer addded

--- quick-list/src/App.js
+++ quick-list/src/App.js
@@ -1,12 +1,34 @@
-import React, {useContext} from 'react'
+import React, {useContext, useReducer} from 'react'
 import {UserContext} from './index'
 
+const initialState = {
+  count: 0
+}
+
+function reducer(state, action) {
+  switch(action.type) {
+    case "increment": 
+      return {
+        count: state.count + 1
+      } 
+    default: 
+      return initialState
+  }
+}
+
 const App = () => {
+
+  const [state, dispatch] = useReducer(reducer,initialState)
 
   const value = useContext(UserContext)
 
   return (
-      <div> Hello,{value}!</div>
+      // <div> Hello,{value}!</div>
+      <div>   
+        count: {state.count}
+        <button onClick={()=> dispatch({type: "increment"})}>Increment</button>
+      
+      </div>
   );
 }
 
