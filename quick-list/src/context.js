8338192898fd616200627488680abcaf805892b9
kcinnoy <yonnick00@gmail.com>
2019-12-02 23:42:09 +0000

    added basic css styling

--- quick-list/src/context.js
+++ quick-list/src/context.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { defaultCipherList } from 'constants';
 
 const TodosContext = React.createContext({
   todos: [
-    {id: 1, text: "Eat Breakfast", complet: false},
-    {id: 1, text: "Eat Breakfast", complet: false},
-    {id: 1, text: "Eat Breakfast", complet: false}
+    {id: 1, text: "Create Delivery plan", complete: false},
+    {id: 2, text: "Write Blog post", complete: false},
+    {id: 3, text: "Draft email template", complete: true}
   ]
 
 })
