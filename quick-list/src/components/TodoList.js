b12d34e2f89b0ccdb8c9186d12e1b64a6c3e6b07
kcinnoy <yonnick00@gmail.com>
2019-12-03 15:16:34 +0000

    added dispatch on span

--- quick-list/src/components/TodoList.js
+++ quick-list/src/components/TodoList.js
@@ -2,7 +2,8 @@ import React, {useContext} from 'react'
 import TodosContext from '../context'
 
 export default function TodoList() {
-  const { state} = useContext(TodosContext);
+  const {state, dispatch} = useContext(TodosContext);
+
   const title = state.todos.length > 0 ? 
     `${state.todos.length} Todos` :
     "Nothing to do!";
@@ -15,7 +16,12 @@ export default function TodoList() {
       <ul className="list-reset text-white p-0">
         {state.todos.map(todo => (
         <li className=" flex items-center bg-orange-600 border-black border-dashed border-2 my-2 py-4" key={todo.id} >
-            <span className="flex-1 ml-12 cursor-pointer"> 
+            <span 
+              className="flex-1 ml-12 cursor-pointer"
+              onDoubleClick={()=> dispatchx({type: "TOGGLE_TODO"})}
+            
+            > 
+              
               {todo.text}
             </span>
             <button>
