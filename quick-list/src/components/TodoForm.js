c08ffbb040149539331e4b733dd38e7c8bd0e851
kcinnoy <yonnick00@gmail.com>
2019-12-11 09:47:59 +0000

    added Add todo action to reducer, added submit todo form functionality

--- quick-list/src/components/TodoForm.js
+++ quick-list/src/components/TodoForm.js
@@ -1,12 +1,27 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
+import TodoContext from '../context'
+import TodosContext from '../context';
+
 
 export default function TodoForm() {
   const [todo, setTodo]= useState("")
+  const {dispatch} = useContext(TodosContext)
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    dispatch({type: "ADD_TODO", payload: todo})
+    setTodo("")
+  }
 
   return (
-    <form className=" flex justify-center p-5">
+    <form onSubmit={handleSubmit} className=" flex justify-center p-5">
 
-      <input type="text" className="border-black border-solid border-2"/>
+      <input 
+        type="text" 
+        className="border-black border-solid border-2"
+        onChange={event => setTodo(event.target.value)}
+        value={todo}
+      />
       
     </form>
   )
