a18ff25883a59d2ded54051e78fd0393340a6976
kcinnoy <yonnick00@gmail.com>
2019-12-13 23:48:10 +0000

    added api to persist data

--- quick-list/src/context.js
+++ quick-list/src/context.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const TodosContext = React.createContext({
   todos: [
-    {id: 1, text: "Create Delivery plan", complete: false},
-    {id: 2, text: "Write Blog post", complete: false},
-    {id: 3, text: "Draft email template", complete: true}
+    // {id: 1, text: "Create Delivery plan", complete: false},
+    // {id: 2, text: "Write Blog post", complete: false},
+    // {id: 3, text: "Draft email template", complete: true}
   ],
   currentTodo: {
 
--- quick-list/src/index.js
+++ quick-list/src/index.js
@@ -1,4 +1,4 @@
-import React, {useContext, useReducer} from 'react';
+import React, {useContext, useReducer, useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
 // import App from './App';
@@ -8,10 +8,35 @@ import * as serviceWorker from './serviceWorker';
 
 import TodoList from './components/TodoList'
 import TodoForm from './components/TodoForm'
+import axios from 'axios';
+
+const useAPI = endpoint => {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  const getData = async () => {
+    const response = await axios.get(endpoint)
+    setData(response.data)
+
+  }
+
+  return data;
+}
 
 export const App = () => {
-  const initialState = useContext(TodosContext)
-  const [state,dispatch] = useReducer(TodosReducer, initialState)
+  const initialState = useContext(TodosContext);
+  const [state,dispatch] = useReducer(TodosReducer, initialState);
+  const savedTodos = useAPI("https://hooks-api-eosin-one.now.sh/todos")
+
+  useEffect(() => {
+    dispatch({
+      type: "GET_TODOS",
+      payload: savedTodos
+    })
+  }, [savedTodos])
 
   return(
     <TodosContext.Provider value= {{state, dispatch}}>
